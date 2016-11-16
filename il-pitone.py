f735ad053b153e2ff612fc96e6ca193ac784f775
D00M3R <raiza.ynos@gmail.com>
2016-11-16 22:36:22 +0100

    Add files via upload

--- il-pitone.py
+++ il-pitone.py
@@ -22,7 +22,7 @@
 #  
 #  
 
-import os, socket, sys, time #Importiamo le librerie
+import os, socket, sys, time , threading#Importiamo le librerie
 
 #Questo crea un oggetto socket
 
@@ -52,24 +52,71 @@ def continuare():
 
 		
 def client_tcp():
-    target_host=raw_input("Inserire il target Host (prova inizialmente con www.google.it): ")
-    target_port=80
-    client=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect((target_host,target_port))
-    client.send("GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
-    response=client.recv(4056)
-    print response
+    try:
+        target_host=raw_input("Inserire il target Host (prova inizialmente con www.google.it): ")
+        target_port=80
+        client=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.connect((target_host,target_port))
+        client.send("GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
+        response=client.recv(4056)
+        print response
+    except Exception as err:
+        print err
 	
-	
+def client_udp():
+    try:
+        target_host=raw_input("Inserire il target Host (prova inizialmente con 127.0.0.1): ")
+        target_port=80
+        client=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        client.sendto("AAABBBCCC",(target_host,target_port))
+        data, addr=client.recvfrom(4096)
+        print data
+    except Exception as err:
+        print err
+        
+def handle_client(client_socket):
+    request = client_socket.recv(1024)
+    print "[*] Received: %s" % request
+    client_socket.send("ACK!")
+    client_socket.close()
+    
+def server_tcp():
+    bind_ip = "0.0.0.0"
+    bind_port = 9999
+    server=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((bind_ip,bind_port))
+    server.listen(5)
+    print "[*] Listening on %s:%d" % (bind_ip,bind_port)
+    while True:
+		client,addr=server.accept()
+		print "[*] Accepted connection from: %s:%d" % (addr[0],addr[1])
+		client_handler=threading.Thread(
+		        target=handle_client,
+		        args=(client,))
+		client_handler.start()
+    
+    
+    
+    
 def main():
     while (f==0):
         wall()
         print"1)Client-TCP"
+        print"2)Client-UDP"	
+        print"3)Server-TCP"
         print"9)Versione"
         print"0)Esci"
         scelta=input("Scelta: ")
         if scelta==1:
             client_tcp()
+            continuare()
+            time.sleep(2)
+        elif scelta==2:
+            client_udp()
+            continuare()
+            time.sleep(2)
+        elif scelta==3:
+            server_tcp()
             continuare()
             time.sleep(2)
         elif scelta==0:
